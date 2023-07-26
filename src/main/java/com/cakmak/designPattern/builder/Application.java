e7d72f1bc28afb8bd9e0561356cbb1fd671377c6
mukail cakmak <tr.mukailcakmak@gmail.com>
2023-07-26 19:32:00 +0300

    [Mukail Cakmak] [main] builder design pattern added to project.

--- src/main/java/com/cakmak/designPattern/builder/Application.java
+++ src/main/java/com/cakmak/designPattern/builder/Application.java
@@ -8,6 +8,10 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+
+		Person person = new Person.Builder().name("Tuğrul").surname("Bayrak").address("Türkiye").build();
+
+		System.out.println(person.getAddress());
 	}
 
 }
